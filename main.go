2d9ee41f3336f68587aa61fe2c0eb640707ea4ad
Dirk Hoekstra <dirkhoekstra@gmail.com>
2019-10-11 12:52:27 +0200

    setup api endpoint

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("The project is setup correctly!")
+	http.HandleFunc("/", homePage)
+
+	fmt.Printf("Server listening on port 8080")
+	log.Panic(http.ListenAndServe(":8080", nil))
+}
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "Welcome to the inspirational quote API homepage")
+	if err != nil {
+		log.Panic(err)
+	}
 }
